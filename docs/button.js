af20b930c6f941d38ea1f936609004ecc3c1f759
Ben Foxall <benfoxall@gmail.com>
2017-09-12 17:33:16 +0100

    Add anime example and add more to the buttons one

--- docs/button.js
+++ docs/button.js
@@ -1,10 +1,12 @@
+const div = document.currentScript.parentElement
+
 const button = document.querySelector('button#a')
 
 // setInterval(() => {
 //   randomcolour()
 // }, 100)
 
-document.body.style.transition = '.5s'
+div.style.transition = '.5s'
 
 button.addEventListener('click', () => {
   console.log("Button was clicked")
@@ -13,7 +15,7 @@ button.addEventListener('click', () => {
 })
 
 function test(t) {
-  document.body.style.background = randomcolour()
+  setBackground(randomcolour())
   if(t) {
     setTimeout(test, 10, t-1)
   }
@@ -38,3 +40,26 @@ document.querySelector('button#c')
  .addEventListener('click', function () {
    this.innerText = Math.random().toString(32)
  }, false)
+
+
+const {style} = document.createElement('span')
+const isColor = (str) => {
+  style.backgroundColor = '#fff'
+  style.color = '#000'
+
+  style.backgroundColor = style.color = str
+  return style.backgroundColor == style.color
+}
+
+
+
+document.querySelector('#color')
+  .addEventListener('keyup', (event) => {
+    const value = event.target.value
+    if(isColor(value)) setBackground(value)
+  })
+
+
+function setBackground (c) {
+  div.style.background = c || randomcolour()
+}
